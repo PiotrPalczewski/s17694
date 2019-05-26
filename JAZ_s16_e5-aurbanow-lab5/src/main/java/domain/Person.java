b5bceee8444177b3073fdd74372ee30badfd299d
PiotrPalczewski <32613484+PiotrPalczewski@users.noreply.github.com>
2019-05-26 17:38:50 +0200

    SklepIT
    zad 6 - występuje błąd dodawania do bazy (str 9 z ćwiczenia na eduxie) - nie tworzy się tabela w HSQL

---
+++ JAZ_s16_e5-aurbanow-lab5/src/main/java/domain/Person.java
@@ -0,0 +1,44 @@
+package domain;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+@Entity
+@NamedQuery(name="person.all", query="SELECT p FROM Person p")
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String surname;
+	private List<Car> cars;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public List<Car> getCars() {
+		return cars;
+	}
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
+	
+}
